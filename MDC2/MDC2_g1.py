d93d4aebbd310ffb6ad2efb6763a53b6879004ab
ark0015 <ark0015@mix.wvu.edu>
2018-11-30 15:48:18 -0500

    Running noise models for all pulsars

--- MDC2/MDC2_g1.py
+++ MDC2/MDC2_g1.py
@@ -1,10 +1,9 @@
 from __future__ import division
 
 import numpy as np
-import glob
+import glob, os, json
 import matplotlib.pyplot as plt
 import scipy.linalg as sl
-import os
 
 import libstempo as libs
 import libstempo.plot as libsplt
@@ -20,17 +19,20 @@ from enterprise.signals import signal_base
 import corner
 from PTMCMCSampler.PTMCMCSampler import PTSampler as ptmcmc
 
+topdir = os.getcwd()
 #Where the original data is
-datadir = os.getcwd() + '/mdc2/group1/dataset_1b'
-#Where the refit par files are
-pardir = os.getcwd() + '/dataset_1b_correctedpars/'
+datadir = topdir + '/mdc2/group1/dataset_1b'
 #Where the json noise file is
-noisefile = os.getcwd() + '/mdc2/group1/challenge1_psr_noise.json'
+noisefile = topdir + '/mdc2/group1/challenge1_psr_noise.json'
+#Where the refit par files are
+pardir = topdir + '/dataset_1b/dataset_1b_correctedpars/'
 #Where the chains should be saved to
 #NEED TO CHANGE OUTDIR FILE ON DIFFERENT RUNS (ie open1 -> open2)
-chaindir = os.getcwd() + '/chains/dataset_1b/'
+chaindir = topdir + '/dataset_1b/chains/'
 #Where we save figures n stuff
-figdir = os.getcwd() + '/Cornerplts/'
+figdir = topdir + '/dataset_1b/Cornerplts/'
+#Where we save new json file
+noisedir = topdir + '/dataset_1b/'
 
 def Refit_pars(origdir,newdir):
 	orig_parfiles = sorted(glob.glob(origdir + '/*.par'))
@@ -44,11 +46,17 @@ def Refit_pars(origdir,newdir):
 	#Fit the par files again
 	#Save them to new directory (Overwrites ones currently used in savedir)
 	for orig_libs_psr in orig_libs_psrs:
+		print('Refitting ' + orig_libs_psr.name)
 		orig_libs_psr['DM'].fit = False
 		orig_libs_psr['DM1'].fit = False
 		orig_libs_psr['DM2'].fit = False
 		orig_libs_psr.fit(iters=10)
 		orig_libs_psr.savepar(newdir + orig_libs_psr.name + '.par')
+		orig_libs_psr.savetim(newdir + orig_libs_psr.name + '.tim')
+		if input('Continue?') == 'y':
+			continue
+		else:
+			break
 
 
 parfiles = sorted(glob.glob(datadir + '/*.par'))
@@ -58,7 +66,6 @@ psrs = []
 for p, t in zip(parfiles, timfiles):
     psr = Pulsar(p, t)
     psrs.append(psr)
-
 
 # find the maximum time span to set GW frequency sampling
 tmin = [p.toas.min() for p in psrs]
@@ -92,9 +99,10 @@ tm = gp_signals.TimingModel()
 # full model is sum of components
 model = ef + rn + tm + eq
 
-noise_params = ["efac", "equad",  "rn_log10_A", "rn_spec_idx"]
+psr_dict = {}
 
 for psr in psrs:
+	print('Working on ' + psr.name)
 	outdir = chaindir + psr.name + '/'
 	if not os.path.exists(outdir):
 		os.mkdir(outdir)
@@ -112,14 +120,14 @@ for psr in psrs:
 	cov = np.diag(np.ones(ndim) * 0.01**2)
 
 	# Now we figure out which indices the red noise parameters have
-	rn_idx1 = pta.param_names.index(psr.name + 'red_noise_log10_equad')
-	rn_idx2 = pta.param_names.index(psr.name + 'red_noise_gamma')
+	rn_idx1 = pta.param_names.index(psr.name + '_red_noise_log10_A')
+	rn_idx2 = pta.param_names.index(psr.name + '_red_noise_gamma')
 
 	# set up jump groups by red noise groups
 	ndim = len(xs)
 	groups  = [range(0, ndim)]
 	groups.extend([[rn_idx1,rn_idx2]])
-As
+
 	# intialize sampler
 	sampler = ptmcmc(ndim, pta.get_lnlikelihood, pta.get_lnprior, cov, groups=groups, outDir=outdir)
 
@@ -139,14 +147,20 @@ As
 		nf.close()
 	#Unpacking dictionaries in json file to get at noise values
 	noise_vals = inputvals[psr.name]
-	truths = [noise_vals["efac"], noise_vals["rn_spec_ind"], noise_vals["rn_log10_A"], noise_vals["equad"]]
+	truths = [noise_vals["efac"], noise_vals["equad"], noise_vals["rn_spec_ind"], noise_vals["rn_log10_A"]]
 
 	#Make corner plots
 	corner.corner(chain[burn:,:-4], 30, truths = truths, labels=pars)
+	#corner.corner(chain, 30, truths = truths, labels=pars)
 	plt.savefig(figdir + psr.name + '_cornerplt.pdf')
 	plt.close()
 
 	#make dictionary of pulsar parameters from these runs
 	psr_dict[psr.name] = {}
-	for p in noise_params:
+	for param_name,idx in zip(pars,range(len(pars))):
+		psr_dict[psr.name][param_name] = np.mean(chain[:,idx])
 
+# Now we want to save this all as a json file
+with open(noisedir+"fit_psr_noise.json", 'w') as fpn:
+    json.dump(psr_dict, fpn)
+    fpn.close()
