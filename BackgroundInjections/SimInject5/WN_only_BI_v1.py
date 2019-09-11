e1643a3f9381e1968e1a931a0e72183a5615491a
Andrew Kaiser <andrewkaiser@Kaiser70.local>
2019-09-11 11:54:47 -0400

    Setting up runs for spruce

--- BackgroundInjections/SimInject5/WN_only_BI_v1.py
+++ BackgroundInjections/SimInject5/WN_only_BI_v1.py
@@ -21,7 +21,7 @@ from PTMCMCSampler.PTMCMCSampler import PTSampler as ptmcmc
 
 import sim_gw as SG
 
-scratch_dir = '/scratch/ark0015/background_injections/'
+scratch_dir = '/scratch/ark0015/background_injections'
 
 runname = '/simGWB_1'
 #Where the everything should be saved to (chains,etc.)
@@ -33,9 +33,9 @@ if os.path.exists(outdir) == False:
     os.mkdir(outdir)
 
 #The pulsars
-psrs_wn_only_dir = backgrouninjection_dir + '/FakePTA/'
-#noise11yr_path = backgrouninjection_dir + '/nano11/noisefiles_new/'
-#psrlist11yr_path = backgrouninjection_dir + '/nano11/psrlist_Tg3yr.txt'
+psrs_wn_only_dir = scratch_dir + '/FakePTA'
+#noise11yr_path = scratch_dir + '/nano11/noisefiles_new'
+#psrlist11yr_path = scratch_dir + '/nano11/psrlist_Tg3yr.txt'
 
 
 
@@ -45,7 +45,7 @@ timfiles = sorted(glob.glob(psrs_wn_only_dir+'*.tim'))
 
 
 # #### Instantiate a "Simulation class"
-sim1 = SG.Simulation(parfiles,timfiles)
+sim = SG.Simulation(parfiles,timfiles)
 
 
 # #### Inject 2 backgrounds
@@ -58,49 +58,55 @@ background_gamma_2 = 7./3.
 background_seed_1 = 1986
 background_seed_2 = 1667
 
-#LT.createGWB(sim1.libs_psrs, A_gwb, gamma_gw, seed=seed)
-LT.createGWB(sim1.libs_psrs,background_amp_1,background_gamma_1,seed=background_seed_1)
-LT.createGWB(sim1.libs_psrs,background_amp_2,background_gamma_2,seed=background_seed_2,noCorr=True)
-#sim1.createGWB(background_amp_1,gamma_gw=background_gamma_1,seed=background_seed_1)
-#sim1.createGWB(background_amp_2,gamma_gw=background_gamma_2,seed=background_seed_2)
+#Save Injection Parameters
+injection_parameters = {}
+injection_parameters['Background_1'] = {'log_10_amp':np.log10(background_amp_1),\
+                                        'gamma':background_gamma_1,\
+                                        'seed':background_seed_1}
+injection_parameters['Background_2'] = {'log_10_amp':np.log10(background_amp_2),\
+                                        'gamma':background_gamma_2,\
+                                        'seed':background_seed_2}
+with open(outdir + '/injection_parameters.json', 'w') as fp:
+    json.dump(injection_parameters, fp, sort_keys=True)
+
+sim.createGWB(background_amp_1,gamma_gw=background_gamma_1,seed=background_seed_1)
+sim.createGWB(background_amp_2,gamma_gw=background_gamma_2,seed=background_seed_2,noCorr=True)
 
 
 # ### Get pulsars as enterprise pulsars
-sim1.init_ePulsars()
+sim.init_ePulsars()
 
 
 # #### Use Simple 2 GWB model to instantiate enterprise PTA
 background_gammas = [background_gamma_1, background_gamma_2]
-pta1 = SG.model_simple_multiple_gwbs(sim1.psrs,gammas=background_gammas)
-#pta1 = SG.model_simple_multiple_gwbs(sim1.psrs,gammas=[background_gamma_2])
-
+pta = SG.model_simple_multiple_gwbs(sim.psrs,gammas=background_gammas)
 
 # #### Save params for plotting
-with open(outdir + '/parameters.json', 'w') as fp:
-    json.dump(pta1.param_names, fp)
+with open(outdir + '/sample_parameters.json', 'w') as fp:
+    json.dump(pta.param_names, fp)
 
 
 # #### Set up sampler and initial samples
 
 #Pick random initial sampling
-xs1 = {par.name: par.sample() for par in pta1.params}
+xs = {par.name: par.sample() for par in pta.params}
 
 # dimension of parameter space
-ndim1 = len(xs1)
+ndim = len(xs)
 
 # initial jump covariance matrix
-cov1 = np.diag(np.ones(ndim1) * 0.01**2)
+cov = np.diag(np.ones(ndim) * 0.01**2)
 
-groups1 = model_utils.get_parameter_groups(pta1)
-groups1.append([ndim1-2,ndim1-1])
+groups = model_utils.get_parameter_groups(pta)
+groups.append([ndim-2,ndim-1])
 
 # intialize sampler
-sampler = ptmcmc(ndim1, pta1.get_lnlikelihood, pta1.get_lnprior, cov1, groups=groups1, outDir = outdir,resume=False)
+sampler = ptmcmc(ndim, pta.get_lnlikelihood, pta.get_lnprior, cov, groups=groups, outDir = outdir,resume=False)
 
 
 # # Sample!
 # sampler for N steps
-N = int(1e5)
-x0 = np.hstack(p.sample() for p in pta1.params)
+N = int(1e2)
+x0 = np.hstack(p.sample() for p in pta.params)
 sampler.sample(x0, N, SCAMweight=30, AMweight=15, DEweight=50)
 
